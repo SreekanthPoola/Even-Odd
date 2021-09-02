db6da2e6002b5af4a026aacdcdb2170222520122
SreekanthPoola <51588236+SreekanthPoola@users.noreply.github.com>
2021-09-02 16:53:49 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+public class Example {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		int n = s.nextInt();
+		if(n%2 ==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+}
+		
